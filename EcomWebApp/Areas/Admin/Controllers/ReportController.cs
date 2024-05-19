750287b2e9f030d27e4cd568fc657c3a982d541a
sanamsitoula <sanam.ctaula@gmail.com>
2024-05-20 00:33:54 +0545

    sss

---
+++ EcomWebApp/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,59 @@
+﻿using Ecom.DataAccess.Data;
+using Ecom.DataAccess.Repository.IRepository;
+using Ecom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
+using Microsoft.SqlServer.Server;
+using System.Net.Sockets;
+
+namespace Ecom.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReportController : Controller
+    {
+      
+        private readonly IUnitOfWork _unitOfWork;
+        public ReportController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult P2MReport()
+        {
+            List<P2M>? objProductList = _unitOfWork.P2M.
+              GetAll()?
+          .Select(P2M => new P2M
+          {
+              Id = P2M.Id,
+              Name = P2M.Name,
+              ProductId = P2M.ProductId,
+              JobStepId = P2M.JobStepId,
+             
+
+
+              // Set values for any other new properties
+          }).OrderByDescending(c => c.Id)
+                                   .ToList();
+
+            if (objProductList != null)
+            {
+                objProductList.ForEach(e =>
+                {
+                    e.Product = new Product // Assuming Product is a complex object
+                    {
+                        Title = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == e.ProductId).Title,
+                        Id = (int)e.ProductId,
+                    };
+                  
+                    
+
+
+                });
+            }
+            return View(objProductList);
+        }
+
+       
+    }
+}
